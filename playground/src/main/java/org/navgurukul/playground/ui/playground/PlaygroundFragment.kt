7756482f0e940941100aff35dcc312ffad097b30
abhishek <abhishek@threewheelsunited.com>
2020-07-30 21:14:11 +0530

    Add chaquopy REPL in Playground module

--- playground/src/main/java/org/navgurukul/playground/ui/playground/PlaygroundFragment.kt
+++ playground/src/main/java/org/navgurukul/playground/ui/playground/PlaygroundFragment.kt
@@ -1,14 +1,15 @@
 package org.navgurukul.playground.ui.playground
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.navgurukul.playground.R
+import org.navgurukul.playground.chaquopy.console.ReplActivity
 
 class PlaygroundFragment : Fragment() {
 
@@ -19,14 +20,12 @@ class PlaygroundFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-      return inflater.inflate(R.layout.fragment_playground, container, false)
+        return inflater.inflate(R.layout.fragment_playground, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val textView: TextView = view.findViewById(R.id.text)
-        viewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val button: Button = view.findViewById(R.id.buttonPythonPlayground)
+        button.setOnClickListener { startActivity(Intent(context, ReplActivity::class.java)) }
     }
 }
