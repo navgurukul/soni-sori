d2be11197463927b1a71446e3a6966658967a0c0
Suhaib Roomy <suhaib.roomy@booking.com>
2020-10-31 14:25:34 +0100

    clickable links in chat

--- chat/src/main/java/org/navgurukul/chat/core/utils/ExternalApplicationsUtil.kt
+++ chat/src/main/java/org/navgurukul/chat/core/utils/ExternalApplicationsUtil.kt
@@ -4,11 +4,40 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.provider.Browser
 import androidx.core.content.FileProvider
 import org.navgurukul.chat.BuildConfig
 import org.navgurukul.chat.R
 import timber.log.Timber
 import java.io.File
+
+/**
+ * Open a url in the internet browser of the system
+ */
+fun openUrlInExternalBrowser(context: Context, url: String?) {
+    url?.let {
+        openUrlInExternalBrowser(context, Uri.parse(it))
+    }
+}
+
+/**
+ * Open a uri in the internet browser of the system
+ */
+fun openUrlInExternalBrowser(context: Context, uri: Uri?) {
+    uri?.let {
+        val browserIntent = Intent(Intent.ACTION_VIEW, it).apply {
+            putExtra(Browser.EXTRA_APPLICATION_ID, context.packageName)
+            putExtra(Browser.EXTRA_CREATE_NEW_TAB, true)
+        }
+
+        try {
+            context.startActivity(browserIntent)
+        } catch (activityNotFoundException: ActivityNotFoundException) {
+            context.toast(R.string.error_no_external_application_found)
+        }
+    }
+}
+
 
 fun shareMedia(context: Context, file: File, mediaMimeType: String?) {
     var mediaUri: Uri? = null
