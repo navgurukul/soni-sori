642d6bff9e934a8c5181460332db183478f0f926
Suhaib Roomy <suhaibroomy91@gmail.com>
2021-04-21 16:04:14 +0200

    Added an option to join class in the Class details screen

--- app/src/main/java/org/merakilearn/EnrollViewModel.kt
+++ app/src/main/java/org/merakilearn/EnrollViewModel.kt
@@ -13,28 +13,50 @@ import org.navgurukul.commonui.platform.ViewModelAction
 import org.navgurukul.commonui.platform.ViewState
 import org.navgurukul.commonui.resources.StringProvider
 
-class EnrollViewModel(private val classId: Int,
-                      private val isEnrolled: Boolean,
-                      private val stringProvider: StringProvider,
-                      private val applicationRepo: ApplicationRepo
-): BaseViewModel<EnrollViewEvents, EnrollViewState>(EnrollViewState()) {
+class EnrollViewModel(
+    private val classId: Int,
+    private var isEnrolled: Boolean,
+    private val stringProvider: StringProvider,
+    private val applicationRepo: ApplicationRepo
+) : BaseViewModel<EnrollViewEvents, EnrollViewState>(EnrollViewState()) {
+
+    private lateinit var meetLink: String
 
     init {
         setState { copy(isLoading = true) }
         viewModelScope.launch {
             val classes = applicationRepo.fetchClassData(classId)
             classes?.let {
-                val buttonText = if (isEnrolled) {
-                    stringProvider.getString(R.string.drop_out)
+                val primaryAction = if (isEnrolled) {
+                    stringProvider.getString(R.string.join_class)
                 } else {
                     stringProvider.getString(R.string.enroll_to_class)
                 }
-                val teacherName =  classes.facilitator?.name ?: stringProvider.getString(R.string.unavailable)
+                val secondaryAction = if (isEnrolled) {
+                    stringProvider.getString(R.string.drop_out)
+                } else {
+                    null
+                }
+                val teacherName =
+                    classes.facilitator?.name ?: stringProvider.getString(R.string.unavailable)
                 val date = "${classes.startTime.toDate()}. (${classes.startTime.toDay()})"
                 val time = "${classes.startTime.toTime()} - ${classes.endTime.toTime()}"
-                val dateAndTiming = stringProvider.getString(R.string.enroll_date_and_time, teacherName, date, time)
-                setState { copy(isLoading = false, showError = false, enrollButton = buttonText,
-                    about = classes.description, details = dateAndTiming, rules = classes.rules?.en, title = classes.title, type = classes.sanitizedType()) }
+                val dateAndTiming =
+                    stringProvider.getString(R.string.enroll_date_and_time, teacherName, date, time)
+                meetLink = classes.meetLink
+                setState {
+                    copy(
+                        isLoading = false,
+                        showError = false,
+                        primaryAction = primaryAction,
+                        secondaryAction = secondaryAction,
+                        about = classes.description,
+                        details = dateAndTiming,
+                        rules = classes.rules?.en,
+                        title = classes.title,
+                        type = classes.sanitizedType()
+                    )
+                }
             } ?: run {
                 setState { copy(isLoading = false, showError = true) }
             }
@@ -43,27 +65,52 @@ class EnrollViewModel(private val classId: Int,
 
     fun handle(viewActions: EnrollViewActions) {
         when (viewActions) {
-            is EnrollViewActions.EnrollToClass -> enrollToClass()
+            is EnrollViewActions.PrimaryAction -> primaryAction()
+            EnrollViewActions.SecondaryAction -> secondaryAction()
         }
     }
 
-    private fun enrollToClass() {
+    private fun secondaryAction() {
         viewModelScope.launch {
             setState { copy(isLoading = true) }
-            val result = applicationRepo.enrollToClass(classId, isEnrolled)
+            val result = applicationRepo.enrollToClass(classId, true)
             setState { copy(isLoading = false) }
-            if (isEnrolled) {
-                if (result) {
-                    _viewEvents.setValue(EnrollViewEvents.ShowToast(stringProvider.getString(R.string.log_out_class)))
-                    _viewEvents.setValue(EnrollViewEvents.CloseScreen)
-                } else {
-                    _viewEvents.setValue(EnrollViewEvents.ShowToast(stringProvider.getString(R.string.unable_to_drop)))
+            if (result) {
+                isEnrolled = false
+                setState {
+                    copy(
+                        isLoading = false,
+                        primaryAction = stringProvider.getString(R.string.enroll_to_class),
+                        secondaryAction = null
+                    )
                 }
+                _viewEvents.setValue(EnrollViewEvents.ShowToast(stringProvider.getString(R.string.log_out_class)))
             } else {
+                setState { copy(isLoading = false) }
+                _viewEvents.setValue(EnrollViewEvents.ShowToast(stringProvider.getString(R.string.unable_to_drop)))
+            }
+        }
+    }
+
+    private fun primaryAction() {
+        viewModelScope.launch {
+            if (isEnrolled) {
+                _viewEvents.setValue(EnrollViewEvents.OpenLink(meetLink))
+            } else {
+                setState { copy(isLoading = true) }
+                val result = applicationRepo.enrollToClass(classId, false)
                 if (result) {
+                    isEnrolled = true
+                    setState {
+                        copy(
+                            isLoading = false,
+                            primaryAction = stringProvider.getString(R.string.join_class),
+                            secondaryAction = stringProvider.getString(R.string.drop_out)
+                        )
+                    }
                     _viewEvents.setValue(EnrollViewEvents.ShowToast(stringProvider.getString(R.string.enrolled)))
-                    _viewEvents.setValue(EnrollViewEvents.CloseScreen)
                 } else {
+                    setState { copy(isLoading = false) }
                     _viewEvents.setValue(EnrollViewEvents.ShowToast(stringProvider.getString(R.string.unable_to_enroll)))
                 }
             }
@@ -73,18 +120,20 @@ class EnrollViewModel(private val classId: Int,
 }
 
 sealed class EnrollViewEvents : ViewEvents {
-    object CloseScreen: EnrollViewEvents()
     class ShowToast(val toastText: String) : EnrollViewEvents()
+    class OpenLink(val link: String) : EnrollViewEvents()
 }
 
 sealed class EnrollViewActions : ViewModelAction {
-    object EnrollToClass : EnrollViewActions()
+    object PrimaryAction : EnrollViewActions()
+    object SecondaryAction : EnrollViewActions()
 }
 
 data class EnrollViewState(
     val isLoading: Boolean = false,
     val showError: Boolean = false,
-    val enrollButton: String? = null,
+    val primaryAction: String? = null,
+    val secondaryAction: String? = null,
     val type: String? = null,
     val about: String? = null,
     val details: String? = null,
