e9e4bb6e4660051212a80b22d1fa68d63a7af993
Ankit Kr <ankit@paynet.pro>
2021-10-02 15:38:36 +0530

    Course list new ui implemented

--- learn/src/main/java/org/navgurukul/learn/ui/learn/adapter/CourseAdapter.kt
+++ learn/src/main/java/org/navgurukul/learn/ui/learn/adapter/CourseAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
+import com.bumptech.glide.Glide
 import org.navgurukul.learn.R
 import org.navgurukul.learn.courses.db.models.Course
 import org.navgurukul.learn.databinding.ItemCourseBinding
@@ -33,6 +34,16 @@ class CourseAdapter(val callback: (Course) -> Unit) :
     override fun bind(holder: DataBoundViewHolder<ItemCourseBinding>, item: Course) {
         val binding = holder.binding
         binding.course = item
+
+        val thumbnail = Glide.with(holder.itemView)
+            .load(R.drawable.ic_typing_icon)
+
+        Glide.with(binding.ivLogo)
+            .load(item.logo)
+            .thumbnail(thumbnail)
+            .into(binding.ivLogo)
+
+        binding.tvName.text = item.name
         binding.root.setOnClickListener {
             callback.invoke(item)
         }
