55120468439642e4b1a1989c5346aabb87162469
admin123 <dhanshri20@navgurukul.org>
2022-03-26 21:10:28 +0530

    minor

--- learn/build.gradle.kts
+++ learn/build.gradle.kts
@@ -80,6 +80,9 @@ dependencies {
     implementation(AndroidxDependencies.roomRuntime)
     implementation(AndroidxDependencies.roomKtx)
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     kapt(AndroidxDependencies.roomCompiler)
 
     implementation(MiscellaneousDependencies.textDrawable)
