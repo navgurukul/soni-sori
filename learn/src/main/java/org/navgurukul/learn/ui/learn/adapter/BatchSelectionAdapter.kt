0fd8aaa9781435db09ebf3921d3099f2b9d4d4a4
admin123 <dhanshri20@navgurukul.org>
2022-04-03 23:59:23 +0530

    batches change

--- learn/src/main/java/org/navgurukul/learn/ui/learn/adapter/BatchSelectionAdapter.kt
+++ learn/src/main/java/org/navgurukul/learn/ui/learn/adapter/BatchSelectionAdapter.kt
@@ -6,6 +6,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import org.navgurukul.learn.R
 import org.navgurukul.learn.courses.network.model.Batch
+import org.navgurukul.learn.courses.network.model.dateRange
 import org.navgurukul.learn.databinding.ItemBatchBinding
 import org.navgurukul.learn.ui.common.DataBoundListAdapter
 
@@ -31,6 +32,8 @@ class BatchSelectionAdapter(val callback: (Batch) -> Unit):
     override fun bind(holder: DataBoundViewHolder<ItemBatchBinding>, item: Batch) {
         val binding = holder.binding
         binding.batch = item
+        binding.tvTitleBatch.text = item.title
+        binding.tvBatchDate.text = item.dateRange()
         binding.root.setOnClickListener {
             callback.invoke(item)
         }
