bd7a78473174a30d10c2ee657c3f41269793fefb
dhanshri <dhanshri20@navgurukul.org>
2022-08-02 14:10:29 +0530

    progress bar

--- app/src/main/java/org/merakilearn/datasource/PlaygroundRepo.kt
+++ app/src/main/java/org/merakilearn/datasource/PlaygroundRepo.kt
@@ -21,7 +21,7 @@ class PlaygroundRepo {
             PlaygroundItemModel(
                 PlaygroundTypes.SCRATCH,
                 name="Scratch",
-                iconResource = R.drawable.ic_icon_typing,
+                iconResource = R.drawable.ic_scratch_cat,
             ),
 
         )
--- app/src/main/java/org/merakilearn/ui/ScratchActivity.kt
+++ app/src/main/java/org/merakilearn/ui/ScratchActivity.kt
@@ -2,11 +2,12 @@ package org.merakilearn.ui
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
+import android.view.View
 import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.parcel.Parcelize
 import org.merakilearn.R
 import org.merakilearn.core.extentions.KEY_ARG
@@ -15,6 +16,7 @@ import org.merakilearn.core.extentions.toBundle
 @Parcelize
 object ScratchActivityArgs: Parcelable
 class ScratchActivity : AppCompatActivity() {
+
         companion object {
         fun start(context: Context) {
             val intent = Intent(context,ScratchActivity::class.java).apply {
@@ -27,20 +29,57 @@ class ScratchActivity : AppCompatActivity() {
         intent.extras?.getParcelable(KEY_ARG)
     }
     private lateinit var webView: WebView
+    lateinit var progressBar:ProgressBar
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scratch)
 
-                webView = findViewById(R.id.webview)
+        webView = findViewById(R.id.webview)
+        progressBar = findViewById(R.id.progressBar)
+        setProgressValue(0)
         webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://scratchdev.merakilearn.org/")
+        webView.loadUrl("https://scratch.merakilearn.org")
 
         webView.settings.javaScriptEnabled = true
 
         webView.settings.setSupportZoom(true)
+
+
     }
+
+    // Overriding WebViewClient functions
+    inner class WebViewClient : android.webkit.WebViewClient() {
+        // Load the URL
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            view.loadUrl(url)
+            return false
+        }
+
+        // ProgressBar will disappear once page is loaded
+        override fun onPageFinished(view: WebView, url: String) {
+            super.onPageFinished(view, url)
+           progressBar.visibility = View.GONE
+        }
+    }
+
+    private fun setProgressValue(progress: Int) {
+
+        // set the progress
+        progressBar.setProgress(progress)
+        // thread is used to change the progress value
+        val thread = Thread {
+            try {
+                Thread.sleep(1000)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+            setProgressValue(progress + 1)
+        }
+        thread.start()
+    }
+
 
     override fun onBackPressed() {
         // if your webview can go back it will go back
@@ -52,3 +91,4 @@ class ScratchActivity : AppCompatActivity() {
             super.onBackPressed()
     }
 }
+
