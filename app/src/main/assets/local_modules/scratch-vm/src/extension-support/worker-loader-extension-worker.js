64c410c60036e263eaf30c25e3c3061c200aefd9
nilesh <neilkumar93600@gmail.com>
2022-10-16 19:37:23 +0530

    Web to Android Link with New Assets.

--- app/src/main/assets/local_modules/scratch-vm/src/extension-support/worker-loader-extension-worker.js
+++
@@ -1,8 +0,0 @@
-/***/ (function (module, exports, __webpack_require__) {
-
-    module.exports = function () {
-        return new Worker(__webpack_require__.p + "extension-worker.js");
-    };
-
-    /***/
-}) 
