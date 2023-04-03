47b9a1316d19ce0dc9c69730feb7d527cbac9ce7
Rahul Raghuwanshi <champrahul202002@gmail.com>
2023-04-03 11:00:57 +0530

    1. internal release 76.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -187,8 +187,8 @@ object BuildConfigVersions {
     const val compileSdkVersion = 31
     const val minSdkVersion = 23
     const val targetSdkVersion = 31
-    const val versionCode = 74
-    const val versionName = "1.5.44"
+    const val versionCode = 76
+    const val versionName = "1.5.46"
     const val applicationId = "org.merakilearn"
 }
 
