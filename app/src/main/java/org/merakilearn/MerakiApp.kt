b2c7e5b657cd97aef268d9639ceefc61cc5a49e5
Abhishek Gupta <abhishekgupta.iitd@gmail.com>
2023-05-18 14:31:10 +0530

    minor changes

--- app/src/main/java/org/merakilearn/MerakiApp.kt
+++ app/src/main/java/org/merakilearn/MerakiApp.kt
@@ -15,8 +15,6 @@ import org.koin.core.context.startKoin
 import org.merakilearn.core.di.coreModules
 import org.merakilearn.core.datasource.Config
 import org.merakilearn.di.appModules
-import org.navgurukul.chat.core.ChatInitializer
-import org.navgurukul.chat.core.di.chatModules
 import org.navgurukul.commonui.di.commonUIModules
 import org.navgurukul.learn.di.learnModules
 import org.navgurukul.playground.di.playgroundModules
@@ -55,7 +53,7 @@ class MerakiApp : Application() {
         val config: Config by inject()
         config.initialise()
 
-        ChatInitializer.initialise(this)
+        //ChatInitializer.initialise(this)
         subscribeToDefaultTopic()
 
         val installReferrerManager: InstallReferrerManager by inject()
