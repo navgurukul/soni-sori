3e0c29a3b49645418727bb7753229f3185432979
dhanshri <dhanshri20@navgurukul.org>
2023-08-04 11:30:12 +0530

    handling thread for svgloader

--- commonUI/src/main/java/org/navgurukul/commonui/platform/SvgLoader.kt
+++ commonUI/src/main/java/org/navgurukul/commonui/platform/SvgLoader.kt
@@ -3,12 +3,15 @@ package org.navgurukul.commonui.platform
 import android.app.Activity
 import android.content.Context
 import android.graphics.drawable.PictureDrawable
+import android.os.AsyncTask
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.caverock.androidsvg.SVG
+import kotlinx.coroutines.NonCancellable.isCancelled
 import java.io.InputStream
+import java.lang.ref.WeakReference
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -18,25 +21,42 @@ class SvgLoader(private val context: Context) {
         .skipMemoryCache(true)
 
     fun loadSvgFromUrl(url: String, targetImageView: ImageView) {
-        val thread = object : Thread() {
-            override fun run() {
-                try {
-                    val urlConnection = URL(url).openConnection() as HttpURLConnection
-                    val inputStream: InputStream = urlConnection.inputStream
-                    val svg = SVG.getFromInputStream(inputStream)
-                    val picture = svg.renderToPicture()
-                    val drawable = PictureDrawable(picture)
-                    (context as? Activity)?.runOnUiThread {
-                        Glide.with(context)
-                            .load(drawable)
-                            .apply(requestOptions)
-                            .into(targetImageView)
-                    }
-                } catch (e: Exception) {
-                    e.printStackTrace()
+        val task = SvgLoadingTask(context, targetImageView, requestOptions)
+        task.execute(url)
+    }
+
+    private class SvgLoadingTask(
+        context: Context,
+        private val targetImageView: ImageView,
+        private val requestOptions: RequestOptions
+    ) : AsyncTask<String, Void, SVG?>() {
+        private val contextReference: WeakReference<Context> = WeakReference(context)
+
+        @Deprecated("Deprecated in Java")
+        override fun doInBackground(vararg urls: String): SVG? {
+            try {
+                val urlConnection = URL(urls[0]).openConnection() as HttpURLConnection
+                val inputStream: InputStream = urlConnection.inputStream
+                return SVG.getFromInputStream(inputStream)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        @Deprecated("Deprecated in Java")
+        override fun onPostExecute(svg: SVG?) {
+            val context = contextReference.get()
+            if (context != null && !isCancelled && svg != null) {
+                val picture = svg.renderToPicture()
+                val drawable = PictureDrawable(picture)
+                if (context is Activity && !context.isFinishing) {
+                    Glide.with(context)
+                        .load(drawable)
+                        .apply(requestOptions)
+                        .into(targetImageView)
                 }
             }
         }
-        thread.start()
     }
 }
