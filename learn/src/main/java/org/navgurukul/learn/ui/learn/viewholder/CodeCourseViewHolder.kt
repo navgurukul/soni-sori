6347733c2b742eb12a01ccaa30132198c4da8c8f
boyakalyani <108295611+boyakalyani@users.noreply.github.com>
2023-08-22 14:15:54 +0530

    output is not displaying

--- learn/src/main/java/org/navgurukul/learn/ui/learn/viewholder/CodeCourseViewHolder.kt
+++ learn/src/main/java/org/navgurukul/learn/ui/learn/viewholder/CodeCourseViewHolder.kt
@@ -1,37 +1,32 @@
 package org.navgurukul.learn.ui.learn.viewholder
 
-import android.text.Editable
+import android.content.Context
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
+import androidx.core.view.isVisible
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.navgurukul.learn.R
 import org.navgurukul.learn.courses.db.models.BaseCourseContent
 import org.navgurukul.learn.courses.db.models.CodeBaseCourseContent
 import org.navgurukul.learn.courses.db.models.CodeType
-import org.navgurukul.learn.courses.network.model.ConstantString
-import com.chaquo.python.Python
-import com.chaquo.python.android.AndroidPlatform
-import org.koin.androidx.viewmodel.compat.ScopeCompat.viewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
-import org.koin.core.parameter.parametersOf
-import org.merakilearn.core.extentions.fragmentArgs
-import org.navgurukul.playground.editor.PythonEditorArgs
-import org.navgurukul.playground.editor.PythonEditorViewActions
-import org.navgurukul.playground.editor.PythonEditorViewModel
+import org.navgurukul.playground.repo.PythonRepository
+import org.navgurukul.playground.repo.PythonRepositoryImpl
 
 class CodeCourseViewHolder(itemView: View) :
     BaseCourseViewHolder(itemView) {
 
     private val codeLayout: ConstraintLayout = populateStub(R.layout.example_editor)
     private val codeTitle: TextView = codeLayout.findViewById(R.id.code_title)
-    private val codeBody: EditText = codeLayout.findViewById(R.id.code_body)
-    private val imageViewPlay: Button = codeLayout.findViewById(R.id.run_btn)
-    private val output: TextView = codeLayout.findViewById(R.id.Actual_outPut)
+    private val codeBody: TextView = codeLayout.findViewById(R.id.code_body)
+    private val imageViewPlay: AppCompatButton = codeLayout.findViewById(R.id.run_btn)
 
 
     override val horizontalMargin: Int
@@ -39,16 +34,9 @@ class CodeCourseViewHolder(itemView: View) :
 
     init {
         super.setHorizontalMargin(horizontalMargin)
-        imageViewPlay.setOnClickListener {
-            val pythonCode = codeBody.text.toString()
-            executePythonCode(pythonCode)
-        }
     }
 
-    fun bindView(
-        item: CodeBaseCourseContent,
-        callback: (BaseCourseContent) -> Unit,
-    ) {
+    fun bindView(item: CodeBaseCourseContent, callback: (BaseCourseContent) -> Unit) {
         super.bind(item)
 
         if (item.title.isNullOrBlank()) {
@@ -61,41 +49,69 @@ class CodeCourseViewHolder(itemView: View) :
         when (item.codeTypes) {
             CodeType.javascript -> {
                 imageViewPlay.visibility = View.GONE
-                output.visibility = View.GONE
             }
             CodeType.python -> {
-                imageViewPlay.visibility = View.VISIBLE
-                output.visibility = View.VISIBLE
+                imageViewPlay.isVisible = true
+                imageViewPlay.setOnClickListener {
+                    val pythonCodeWithBreaks = item.value  // Get the Python code from your data
+                    val pythonCode = cleanPythonCode(pythonCodeWithBreaks)
+                    val outputTextView = codeLayout.findViewById<TextView>(R.id.Actual_outPut)
+
+                    // Clear the output TextView
+                    outputTextView.text = ""
+
+                    // Access the context from the itemView
+                    val context = itemView.context
+
+                    GlobalScope.launch(Dispatchers.IO) {
+                        val output = runPythonCode(context, pythonCode)
+                        if (output.isNotEmpty()) {
+                            outputTextView.text = output
+                        }
+                    }
+                }
+
+
+            }
+            else -> {
+                imageViewPlay.visibility = View.GONE
             }
         }
 
+        codeBody.text = HtmlCompat.fromHtml(
+            item.value
+                ?: "", HtmlCompat.FROM_HTML_MODE_COMPACT
+        )
 
-            // Set the code body
-                val editableText = Editable.Factory.getInstance().newEditable(item.value)
-            editableText.replace(
-                0, editableText.length,
-                editableText.toString().replace(ConstantString.LINE_BREAK, ConstantString.LINE_BR_REPLACEMENT)
-                    .replace(ConstantString.EMSP, ConstantString.EMSP_REPLACEMENT)
-            )
-                    codeBody.text = editableText
-        }
+    }
+    private fun cleanPythonCode(codeWithBreaks: String?): String {
+        // Remove HTML line breaks ("<br>") from the Python code
+        return codeWithBreaks?.replace("<br>", "\n") ?: ""
+    }
 
-    private fun executePythonCode(pythonCode: String) {
-        if (!Python.isStarted()) {
-            Python.start(AndroidPlatform(itemView.context))
-        }
-
-        val python = Python.getInstance()
-        val pythonInterpreter = python.getModule("chaquopy.main")
-
+    private suspend fun runPythonCode(context: Context, code: String?): String {
         try {
-            val outputs = pythonInterpreter.callAttr("main", pythonCode)
-            output.text = outputs.toString()
+            Log.d("PythonExecution", "Executing Python code:\n$code")
+
+            // Access shared preferences and other dependencies using the provided context
+            val sharedPreferences = context.getSharedPreferences("YourPreferencesName", Context.MODE_PRIVATE)
+
+            // Initialize PythonRepository and call the runCode method
+            val pythonRepo = PythonRepositoryImpl(sharedPreferences, context)
+
+            // Execute Python code and get the output
+            val output = pythonRepo.runCode(code ?: "", "your_tag") ?: ""
+
+            Log.d("PythonExecution", "Python code executed successfully. Output:\n$output")
+
+            return output  // Return the actual Python code execution output
         } catch (e: Exception) {
-            output.text = "Error executing Python code:\n${e.message}"
             Log.e("PythonExecution", "Error executing Python code", e)
+            // Handle the error here, e.g., return an error message
+            return "Error executing Python code: ${e.message}"
         }
     }
 
 
 }
+
