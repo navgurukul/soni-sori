fcd71b69ebc424c0b1e630cd41ccb262e72f02c0
Aayushi <aayushitiwari21@navgurukul.org>
2023-10-04 15:04:40 +0530

    write comment under some function

--- core/src/main/java/org/merakilearn/core/appopen/AppOpenDelegateImpl.kt
+++ core/src/main/java/org/merakilearn/core/appopen/AppOpenDelegateImpl.kt
@@ -4,10 +4,10 @@ import android.app.Activity
 
 class AppOpenDelegateImpl:AppOpenDelegate {
     override fun onAppOpened(activity: Activity, clearNotifications: Boolean) {
-
+            // Do nothing
     }
 
     override fun onHomeScreenOpened(activity: Activity, clearNotifications: Boolean) {
-
+        // Do nothing
     }
 }
