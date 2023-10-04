97fd84680c156f99744edc241df56bd6399c04be
Aayushi <aayushitiwari21@navgurukul.org>
2023-10-04 18:02:51 +0530

    Deleted The Commented Classes

--- chat/src/main/java/org/navgurukul/chat/features/home/HomeRoomListDataSource.kt
+++
@@ -1,6 +0,0 @@
-package org.navgurukul.chat.features.home
-
-//import org.matrix.android.sdk.api.session.room.model.RoomSummary
-//import org.navgurukul.chat.core.utils.BehaviorDataSource
-//
-//class HomeRoomListDataSource : BehaviorDataSource<List<RoomSummary>>()
