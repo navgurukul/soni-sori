00b09421267b7de03dfc4486dd0d8358ec56a6d0
Aayushi <aayushitiwari21@navgurukul.org>
2024-05-11 22:38:57 +0530

    Commented ChatInitializer code in MerakiApp.kt class as there is no use of it.

--- app/src/main/java/org/merakilearn/MerakiApp.kt
+++ app/src/main/java/org/merakilearn/MerakiApp.kt
@@ -15,7 +15,7 @@ import org.koin.core.context.startKoin
 import org.merakilearn.core.di.coreModules
 import org.merakilearn.core.datasource.Config
 import org.merakilearn.di.appModules
-import org.navgurukul.chat.core.ChatInitializer
+//import org.navgurukul.chat.core.ChatInitializer
 import org.navgurukul.chat.core.di.chatModules
 import org.navgurukul.commonui.di.commonUIModules
 import org.navgurukul.learn.di.learnModules
@@ -55,8 +55,8 @@ class MerakiApp : Application() {
         val config: Config by inject()
         config.initialise()
 
-        ChatInitializer.initialise(this)
-        subscribeToDefaultTopic()
+//        ChatInitializer.initialise(this)
+//        subscribeToDefaultTopic()
 
         val installReferrerManager: InstallReferrerManager by inject()
         installReferrerManager.checkReferrer()
