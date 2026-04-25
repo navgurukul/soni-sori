4c70e8e61dbc75cfed23b1b911df2b5fd0c54f90
boyakalyani <kalyani143jk@gmail.com>
2026-04-25 15:02:21 +0530

    Fix build after edge-to-edge rollout

--- webIDE/src/main/java/org/navgurukul/webide/ui/activity/ProjectActivity.kt
+++ webIDE/src/main/java/org/navgurukul/webide/ui/activity/ProjectActivity.kt
@@ -23,7 +23,6 @@ import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.snackbar.Snackbar
-import org.navgurukul.commonui.platform.setupEdgeToEdge
 import org.navgurukul.webide.R
 import org.navgurukul.webide.databinding.*
 import org.navgurukul.webide.extensions.*
@@ -44,7 +43,7 @@ import org.navgurukul.webide.util.project.ProjectManager
 import org.navgurukul.webide.util.ui.Styles
 import java.io.File
 
-class ProjectActivity : BaseActivity() {
+class ProjectActivity : ThemedActivity() {
 
     private lateinit var projectViewModel: ProjectViewModel
 
@@ -73,8 +72,6 @@ class ProjectActivity : BaseActivity() {
             setTheme(Styles.getThemeInt(this))
             binding = ActivityProjectBinding.inflate(layoutInflater)
             setContentView(binding.root)
-            setupEdgeToEdge()
-            setupEdgeToEdge()
 
             prefs = defaultPrefs(this)
             props = HtmlParser.getProperties(this, projectName)
